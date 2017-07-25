373ca080549b5f9848b8cc2bc57b45f354748705
Andrew Pielage <pandrex247@hotmail.com>
2017-07-25 11:21:35 +0100

    Shorten datasourcedefinition-* paths

---
+++ jpa/datasourcedef-annotation/src/main/java/org/javaee7/jpa/datasourcedef_annotation/config/DataSourceDefinitionConfig.java
@@ -0,0 +1,12 @@
+package org.javaee7.jpa.datasourcedef_annotation.config;
+
+import javax.annotation.sql.DataSourceDefinition;
+import javax.ejb.Stateless;
+
+@DataSourceDefinition(
+    name = "java:app/MyApp/MyDS",
+    className = "org.h2.jdbcx.JdbcDataSource",
+    url = "jdbc:h2:mem:test")
+@Stateless
+public class DataSourceDefinitionConfig {
+}
